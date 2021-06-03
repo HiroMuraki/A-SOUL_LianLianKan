abfffa6cb705475f1b620cbd667b67b70ad1e132
HiroMuraki <419734782@qq.com>
2021-06-03 19:24:41 +0800

    update sound player

---
+++ DianaLLK_GUI/GameSoundPlayer.cs
@@ -0,0 +1,142 @@
+﻿using LianLianKan;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Media;
+
+namespace DianaLLK_GUI {
+    public class GameSoundPlayer {
+        private static readonly string _gameSoundDirectory = "ASSounds";
+        private static GameSoundPlayer _singletonObject;
+        private static object _singletonLocker = new object();
+        private readonly Random _rnd;
+        private readonly List<Uri> _clickFXSounds; // 点击音效
+        private readonly List<Uri> _matchedFXSounds; // 连接成功音效
+        private readonly List<Uri> _gameCompletedSounds; // 结算音效
+        private readonly List<Uri> _gameMusic; // 背景音乐
+        private readonly List<Uri> _avaSkillSounds; // 向晚技能音效
+        private readonly List<Uri> _bellaSkillSounds; // 贝拉技能音效
+        private readonly List<Uri> _carolSkillSounds; // 珈乐技能音效
+        private readonly List<Uri> _dianaSkillSounds; // 嘉然技能音效
+        private readonly List<Uri> _eileenSkillSounds; // 乃琳技能音效
+        private readonly MediaPlayer _clickSoundPlayer; // 点击技能音播放器
+        private readonly MediaPlayer _matchedkSoundPlayer; // 点击技能音播放器
+        private readonly MediaPlayer _musicSoundPlayer; // 背景音乐播放器
+        private readonly MediaPlayer _gameCompletedSoundPlayer; // 结算音播放器
+        private readonly MediaPlayer _skillActivedSoundPlayer; // 技能启动音播放器
+
+        private GameSoundPlayer() {
+            _rnd = new Random();
+
+            _clickSoundPlayer = new MediaPlayer();
+            _matchedkSoundPlayer = new MediaPlayer();
+            _musicSoundPlayer = new MediaPlayer();
+            _gameCompletedSoundPlayer = new MediaPlayer();
+            _skillActivedSoundPlayer = new MediaPlayer();
+
+            _clickFXSounds = new List<Uri>();
+            _matchedFXSounds = new List<Uri>();
+            _gameCompletedSounds = new List<Uri>();
+            _avaSkillSounds = new List<Uri>();
+            _bellaSkillSounds = new List<Uri>();
+            _carolSkillSounds = new List<Uri>();
+            _dianaSkillSounds = new List<Uri>();
+            _eileenSkillSounds = new List<Uri>();
+            _gameMusic = new List<Uri>();
+
+            if (!Directory.Exists(_gameSoundDirectory)) {
+                return;
+            }
+            foreach (var file in Directory.GetFiles(_gameSoundDirectory)) {
+                string fileName = Path.GetFileName(file).ToUpper();
+                Uri soundUri = new Uri($@"{_gameSoundDirectory}\{fileName}", UriKind.Relative);
+                // 跳过非MP3
+                if (!(Path.GetExtension(fileName) == ".MP3")) {
+                    continue;
+                }
+                //
+                if (fileName.StartsWith("CLICK")) {
+                    _clickFXSounds.Add(soundUri);
+                }
+                else if (fileName.StartsWith("COMPLETED")) {
+                    _gameCompletedSounds.Add(soundUri);
+                }
+                else if (fileName.StartsWith("MUSIC")) {
+                    _gameMusic.Add(soundUri);
+                }
+                else if (fileName.StartsWith("MATCHED")) {
+                    _matchedFXSounds.Add(soundUri);
+                }
+                else if (fileName.StartsWith("#AVA")) {
+                    _avaSkillSounds.Add(soundUri);
+                }
+                else if (fileName.StartsWith("#BELLA")) {
+                    _bellaSkillSounds.Add(soundUri);
+                }
+                else if (fileName.StartsWith("#CAROL")) {
+                    _carolSkillSounds.Add(soundUri);
+                }
+                else if (fileName.StartsWith("#DIANA")) {
+                    _dianaSkillSounds.Add(soundUri);
+                }
+                else if (fileName.StartsWith("#EILEEN")) {
+                    _eileenSkillSounds.Add(soundUri);
+                }
+            }
+        }
+        public static GameSoundPlayer GetInstance() {
+            if (_singletonObject == null) {
+                lock (_singletonLocker) {
+                    if (_singletonObject == null) {
+                        _singletonObject = new GameSoundPlayer();
+                    }
+                }
+            }
+            return _singletonObject;
+        }
+
+        public void PlayClickFXSond() {
+            RandomPlay(_clickSoundPlayer, _clickFXSounds);
+        }
+        public void PlayMatchedFXSound() {
+            RandomPlay(_matchedkSoundPlayer, _matchedFXSounds);
+        }
+        public void PlayMusic() {
+            RandomPlay(_musicSoundPlayer, _gameMusic);
+        }
+        public void PlayGameCompletedSound() {
+            RandomPlay(_gameCompletedSoundPlayer, _gameCompletedSounds);
+        }
+        public void PlaySkillActivedSound(LLKSkill skill) {
+            switch (skill) {
+                case LLKSkill.None:
+                    break;
+                case LLKSkill.AvaPower:
+                    RandomPlay(_skillActivedSoundPlayer, _avaSkillSounds);
+                    break;
+                case LLKSkill.BellaPower:
+                    RandomPlay(_skillActivedSoundPlayer, _bellaSkillSounds);
+                    break;
+                case LLKSkill.CarolPower:
+                    RandomPlay(_skillActivedSoundPlayer, _carolSkillSounds);
+                    break;
+                case LLKSkill.DianaPower:
+                    RandomPlay(_skillActivedSoundPlayer, _dianaSkillSounds);
+                    break;
+                case LLKSkill.EileenPower:
+                    RandomPlay(_skillActivedSoundPlayer, _eileenSkillSounds);
+                    break;
+                default:
+                    break;
+            }
+            _skillActivedSoundPlayer.Play();
+        }
+        private void RandomPlay(MediaPlayer player, ICollection<Uri> resources) {
+            if (resources.Count <= 0) {
+                return;
+            }
+            player.Open(_gameCompletedSounds[_rnd.Next(0, resources.Count)]);
+            player.Play();
+        }
+    }
+}
