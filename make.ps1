e5e807a3a516f4b6e4cb4bd2dd1ca8cfca2c5959
HiroMuraki <419734782@qq.com>
2021-06-06 12:28:42 +0800

    update build script

--- make.ps1
+++ make.ps1
@@ -1,2 +1,6 @@
 dotnet publish -r win10-x64 -c release --self-contained=False /p:PublishSingleFile=True -o "OutputProgram"
+$hasPath = Test-Path "OutputProgram/ASoul连连看.exe"
+if ($hasPath) {
+    move -force "OutputProgram/ASoul连连看.exe" "OutputProgram\Tokens\ASoul连连看.exe"
+}
 ren "OutputProgram\DianaLLK_GUI.exe" "ASoul连连看.exe"
