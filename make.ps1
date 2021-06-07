8af09ac17ca544865139db5e1df6c6aa2630441c
HiroMuraki <419734782@qq.com>
2021-06-07 15:11:28 +0800

    update build script

--- make.ps1
+++ make.ps1
@@ -1,6 +1,6 @@
 dotnet publish -r win10-x64 -c release --self-contained=False /p:PublishSingleFile=True -o "OutputProgram"
 $hasPath = Test-Path "OutputProgram/ASoul连连看.exe"
 if ($hasPath) {
-    move -force "OutputProgram/ASoul连连看.exe" "OutputProgram\Tokens\ASoul连连看.exe"
+    Move-Item -force "OutputProgram/ASoul连连看.exe" "OutputProgram\Tokens\ASoul连连看.exe"
 }
-ren "OutputProgram\DianaLLK_GUI.exe" "ASoul连连看.exe"
+Rename-Item "OutputProgram\DianaLLK_GUI.exe" "ASoul连连看.exe"
